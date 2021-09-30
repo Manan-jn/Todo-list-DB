4ce519f19125d0a478d8003366e5faa43dde0e8d
Manan Jain <manan.nitdl@gmail.com>
2021-09-30 17:28:36 +0530

    Ready to deploy

--- app.js
+++ app.js
@@ -131,6 +131,6 @@ app.get("/:customListName", function (req, res) {
     });
 });
 
-app.listen(3000, function () {
-    console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server started SUCCESSFULLY");
 })
